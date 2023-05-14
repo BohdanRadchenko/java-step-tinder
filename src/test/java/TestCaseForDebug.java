98efd2f25fff3b0832695138b5305d82233a411b
Bohdan <radchenkobs@gmail.com>
2023-05-14 17:20:02 +0300

    add junit

---
+++ src/test/java/TestCaseForDebug.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCaseForDebug {
+    @Test
+    public void testCase() {
+        assertEquals("Test", 2, 2);
+    }
+}
